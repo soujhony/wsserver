a27dde7e0ede42b0230718bdafb23640a28b4c93
Jhony Pereira <jhonystein@gmail.com>
2021-07-09 16:47:27 -0300

    Ajustes

--- server.js
+++ server.js
@@ -10,18 +10,9 @@ app.get('/', (req, res) => {
 });
 
 io.of('streaming').on('connection', (socket) => {
-  socket.on('request', (data) => {
-    console.log(data)
-    socket.broadcast.emit('register', data)
-  })
-  socket.on('video', ({ seq, sockets, frame }) => {
-    // console.log(seq, sockets)
-    for (const socketId of sockets) {
-      socket.to(socketId).emit('video', frame)
-    }
-  })
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('unregister', { socketId: socket.id })
+  socket.on('video', ({ seq, frame }) => {
+    console.log(frame)
+    socket.broadcast.emit('stream', { frame, seq })
   })
 });
 
